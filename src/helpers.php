6376c0ca6e8e5640f7e91312d0010e837ed7aa5e
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2025-01-25 14:02:36 -0300

    chore: add counts support

--- src/helpers.php
+++ src/helpers.php
@@ -4,3 +4,33 @@ function command_exists($command): bool
 {
     return ! empty(shell_exec(sprintf('which %s', escapeshellarg($command))));
 }
+
+function delete_count_file(): void
+{
+    $file = get_count_file();
+
+    if (file_exists($file)) {
+        unlink($file);
+    }
+}
+
+function get_count_file($file = '.count'): string
+{
+    return __DIR__.'/../'.$file;
+}
+
+function update_count(int $count): void
+{
+    file_put_contents(get_count_file(), $count);
+}
+
+function read_count(): int
+{
+    $file = get_count_file();
+
+    if (! file_exists($file)) {
+        return 0;
+    }
+
+    return (int) file_get_contents($file);
+}
--- tests/HelpersTest.php
+++ tests/HelpersTest.php
@@ -7,3 +7,35 @@ it('knows the command exists', function () {
     expect(command_exists('not-exists'))
         ->toBeFalse();
 });
+
+describe('it knows the runs count', function () {
+    test('delete count file', function () {
+        delete_count_file();
+
+        expect(read_count())
+            ->toBe(0);
+
+        expect(file_exists(get_count_file()))
+            ->toBeFalse();
+    });
+
+    test('when file does not exists it returns 0', function () {
+        update_count(0);
+
+        expect(read_count())
+            ->toBe(0);
+
+        expect(file_exists(get_count_file()))
+            ->toBeTrue();
+    });
+
+    test('when file exists it returns number', function () {
+        update_count(10);
+
+        expect(read_count())
+            ->toBe(10);
+
+        expect(file_exists(get_count_file()))
+            ->toBeTrue();
+    });
+});
