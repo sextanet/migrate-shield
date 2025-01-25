3a1c93be69d09509feeb40908319898ef76ff27d
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2025-01-25 18:45:15 -0300

    feat: add hardcoded url share funcionality

--- src/Commands/ShieldCountCommand.php
+++ src/Commands/ShieldCountCommand.php
@@ -2,6 +2,8 @@
 
 namespace SextaNet\MigrateShield\Commands;
 
+use SextaNet\MigrateShield\MigrateShield;
+
 class ShieldCountCommand extends Command
 {
     public $signature = 'shield:count';
@@ -15,6 +17,8 @@ class ShieldCountCommand extends Command
         $this->commandInfo("We've protected you ".singular_or_plural(read_count(), 'time', 'times'));
 
         if ($this->confirm("Do you want to share the counter with Laravel Shield's homepage?", true)) {
+            MigrateShield::shareCount();
+
             $this->commandInfo('You counter was shared successfully');
         }
 
--- src/MigrateShield.php
+++ src/MigrateShield.php
@@ -2,4 +2,19 @@
 
 namespace SextaNet\MigrateShield;
 
-class MigrateShield {}
+use Illuminate\Support\Facades\Http;
+
+class MigrateShield
+{
+    public static function getShareCountUrl()
+    {
+        return 'https://sextanet.com/api/packages/migrate-shield/share';
+    }
+
+    public static function shareCount()
+    {
+        return Http::post(self::getShareCountUrl(), [
+            'count' => read_count(),
+        ]);
+    }
+}
