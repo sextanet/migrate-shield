d62ddfd33403ae8a011bed6bdfe52d12740d8b12
Seba Carrasco Poblete <sebacarrasco93@gmail.com>
2025-01-25 19:04:49 -0300

    feat: add custom share url

--- src/MigrateShield.php
+++ src/MigrateShield.php
@@ -6,15 +6,22 @@ use Illuminate\Support\Facades\Http;
 
 class MigrateShield
 {
-    public static function getShareCountUrl()
+    public static function getShareCountUrl(): string
     {
-        return 'https://sextanet.com/api/packages/migrate-shield/share';
+        return config('migrate-shield.share_url')
+            ?? 'https://sextanet.com/api/packages/migrate-shield/share';
     }
 
     public static function shareCount()
     {
+        Http::globalRequestMiddleware(fn ($request) => $request->withHeader(
+            'User-Agent', 'Migrate Shield'
+        ));
+
         return Http::post(self::getShareCountUrl(), [
             'count' => read_count(),
+            'php_version' => phpversion(),
+            'laravel_version' => app()->version(),
         ]);
     }
 }
